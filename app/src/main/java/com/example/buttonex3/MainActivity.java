9f15daf9406ae812633c6dd10b08fd77e59d19c1
ay4748 <ay4748@bs.amalnet.k12.il>
2023-12-15 21:24:08 +0200

    Final!

--- app/src/main/java/com/example/buttonex3/MainActivity.java
+++ app/src/main/java/com/example/buttonex3/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.buttonex3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    int counter=0;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dodge(View view) {
+        btn=findViewById(R.id.button);
+        counter+=1;
+        if(counter==7)
+        {
+            btn.setText("Enough to click. Go to new start!");
+            counter=0;
+        }
+        else
+        {
+            btn.setText("This is a click number:"+counter);
+        }
+
     }
 }
